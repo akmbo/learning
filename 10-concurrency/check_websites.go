19e3d46a7dd8a681992f936d47958062cedb652b
Alex <67482112+akmbo@users.noreply.github.com>
2024-07-18 14:22:45 +0000

    Make CheckWebsites concurrent with wait groups

--- 10-concurrency/check_websites.go
+++ 10-concurrency/check_websites.go
@@ -1,13 +1,27 @@
 package concurrency
 
+import "sync"
+
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+	wg.Add(len(urls))
+
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			result := wc(u)
+			mu.Lock()
+			results[u] = result
+			mu.Unlock()
+			wg.Done()
+		}(url)
 	}
+
+	wg.Wait()
 
 	return results
 }
