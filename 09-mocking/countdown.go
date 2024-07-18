38192889ddc29c0e86b1360579fa4a30db005fff
Alex <67482112+akmbo@users.noreply.github.com>
2024-07-18 13:31:39 +0000

    Create countdown function and basic test with mocking

---
+++ 09-mocking/countdown.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintf(out, "%d\n", i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
